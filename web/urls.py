423faccab9c7020197d1429dd4569d6672c69ae6
Marc Altmann <marcaltmann@posteo.de>
2024-03-17 11:28:02 +0100

    Add upload and download pages

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "web"
 
 urlpatterns = [
-    path('', views.welcome, name="welcome"),
+    path("", views.welcome, name="welcome"),
+    path("upload/", views.upload, name="upload"),
+    path("download/", views.download, name="download"),
 ]
--- web/views.py
+++ web/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def welcome(request):
     return render(request, 'web/welcome.html')
+
+def upload(request):
+    return render(request, "web/upload.html")
+
+def download(request):
+    return render(request, "web/download.html")
